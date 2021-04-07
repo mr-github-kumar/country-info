651e9818873b8a077eb8c5becbc743814bf45e17
Shishir Kumar <imskool30@gmail.com>
2021-04-07 16:32:47 -0700

    test1

--- src/component/Header.js
+++ src/component/Header.js
@@ -6,7 +6,7 @@ function Header({action}) {
         <section className="header fixed z-10 w-full h-20 p-4 flex justify-center items-center bg-light text-dark-2 dark:bg-dark-2 dark:text-light ">
           <div className="max-w-6xl w-full flex justify-between">
             <span className="font-bold text-lg">
-                Where in the world?
+                Where in the world ?
             </span>
             <div className="font-medium flex gap-2 items-center">
               <input type="checkbox" id="toggle" className="hidden"/>
